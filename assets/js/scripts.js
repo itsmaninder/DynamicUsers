4620516fdaa0aae0c6e8d98101ec488db5bbbaa5
Maninder Singh <ermaninder1112@gmail.com>
2023-09-26 19:44:38 +0530

    User card UI updated

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,36 +4,43 @@ function displayCocktail(data) {
 
   for (let key in dataVal) {
     let value = dataVal[key];
+
+    const item = document.createElement("div");
+    item.setAttribute("class", "item");
+
+    const itemHead = document.createElement("div");
+
+    itemHead.setAttribute("class", "item-head");
+    const itemBody = document.createElement("div");
+    itemBody.setAttribute("class", "item-body");
     //image
     let image = document.createElement("img");
     image.setAttribute("src", value.picture.large);
-    divWrap.appendChild(image);
-    //sub
-    let sub = document.createElement("p");
-    sub.innerHTML = value.nat;
-    divWrap.appendChild(sub);
+    itemHead.appendChild(image);
+    
     //name
     let name = document.createElement("h3");
-    name.innerHTML = `Hey I'm ` + value.name.first + " " + value.name.last;
-    divWrap.appendChild(name);
+    name.innerHTML = `Hey I'm <br/>` + value.name.first + " " + value.name.last;
+    itemBody.appendChild(name);
     //address
     let addr = document.createElement("p");
-    addr.innerHTML =value.location.city +
+    addr.innerHTML = `<i class="fa fa-map-marker"></i>` + value.location.city +
       ", " +
-      value.location.state +
-      "<br/>" +
-      value.location.country;
-    divWrap.appendChild(addr);
+      value.location.state;
+      itemBody.appendChild(addr);
     //email
     let email = document.createElement("a");
-    email.setAttribute("href", `mailto:value.email`);
-    email.innerHTML = "<b>Email me</b> : " + value.email;
-    divWrap.appendChild(email);
+    email.setAttribute("href", "mailto:"+value.email);
+    email.innerHTML = `<i class="fa fa-envelope"></i>` + value.email;
+    itemBody.appendChild(email);
     //phone
     let phone = document.createElement("a");
-    phone.setAttribute("href", `tel:value.phone`);
-    phone.innerHTML = "<b>Call On</b> : " + value.phone;
-    divWrap.appendChild(phone);
+    phone.setAttribute("href", "tel:"+value.phone);
+    phone.innerHTML = `<i class="fa fa-phone"></i>` + value.phone;
+    itemBody.appendChild(phone);
+    divWrap.appendChild(item);
+    item.appendChild(itemHead);
+    item.appendChild(itemBody);
   }
 }
 
