d0c2baf8a638b72f9b457fb5f5d62cc10525694b
Maninder Singh <ermaninder1112@gmail.com>
2023-09-26 20:03:01 +0530

    Js updates for User Card

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,5 @@
+// Data setup
+
 function displayCocktail(data) {
   const dataVal = data.results;
   const divWrap = document.getElementById("wrap");
@@ -17,25 +19,27 @@ function displayCocktail(data) {
     let image = document.createElement("img");
     image.setAttribute("src", value.picture.large);
     itemHead.appendChild(image);
-    
+
     //name
     let name = document.createElement("h3");
     name.innerHTML = `Hey I'm <br/>` + value.name.first + " " + value.name.last;
     itemBody.appendChild(name);
     //address
     let addr = document.createElement("p");
-    addr.innerHTML = `<i class="fa fa-map-marker"></i>` + value.location.city +
+    addr.innerHTML =
+      `<i class="fa fa-map-marker"></i>` +
+      value.location.city +
       ", " +
       value.location.state;
-      itemBody.appendChild(addr);
+    itemBody.appendChild(addr);
     //email
     let email = document.createElement("a");
-    email.setAttribute("href", "mailto:"+value.email);
+    email.setAttribute("href", "mailto:" + value.email);
     email.innerHTML = `<i class="fa fa-envelope"></i>` + value.email;
     itemBody.appendChild(email);
     //phone
     let phone = document.createElement("a");
-    phone.setAttribute("href", "tel:"+value.phone);
+    phone.setAttribute("href", "tel:" + value.phone);
     phone.innerHTML = `<i class="fa fa-phone"></i>` + value.phone;
     itemBody.appendChild(phone);
     divWrap.appendChild(item);
@@ -43,6 +47,8 @@ function displayCocktail(data) {
     item.appendChild(itemBody);
   }
 }
+
+// Random Color
 
 function rg(m, n) {
   return Math.floor(Math.random() * (n - m + 1)) + m;
@@ -64,4 +70,42 @@ function randColor() {
   );
 }
 const bg = document.getElementById("body");
-bg.style.setProperty('--color', randColor());
+bg.style.setProperty("--color", randColor());
+
+//  Create a loader element
+
+const loader = document.createElement("div");
+loader.setAttribute("class", "loader");
+
+const body = document.getElementById("body");
+body.appendChild(loader);
+
+//  Fetch data from the API
+fetch("https://randomuser.me/api/")
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then((data) => {
+    console.log(data);
+    //  Remove the loader once data is fetched
+    body.removeChild(loader);
+
+    const mainDiv = document.createElement("div");
+
+    mainDiv.setAttribute("id", "wrap");
+    body.appendChild(mainDiv);
+    displayCocktail(data);
+  })
+  .catch((error) => {
+    //  Remove the loader in case of an error
+    body.removeChild(loader);
+    console.error("FETCH ERROR:", error);
+  });
+
+setTimeout(function() {
+  history.go(0);
+}, 5000);
